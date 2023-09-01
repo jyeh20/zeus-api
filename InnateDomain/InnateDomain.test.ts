8c2275ec9406edced4bc5c64aede05b186d11aae
jyeh20 <jmyeh51@gmail.com>
2023-08-31 22:41:11 -0700

    Removed mock ip map, moved TEST env conditional map to consts file instead. Wrote unit tests for Innate Domain constructor and fetchProperty method

--- InnateDomain/InnateDomain.test.ts
+++ InnateDomain/InnateDomain.test.ts
@@ -1,12 +1,6 @@
 import { describe, expect, test } from "@jest/globals";
 import InnateDomain from "./InnateDomain";
 
-const ip_map_for_testing = {
-  "192.168.4.2": 0,
-  "192.168.4.3": 1,
-  "192.168.6.2": 2,
-};
-
 // setup mock Domains
 
 const domain1 = new InnateDomain("192.168.4.2");
@@ -16,5 +10,39 @@ const domain3 = new InnateDomain("192.168.6.2");
 describe("Innate Domain Constructor", () => {
   test("Using a valid IP", () => {
     expect(new InnateDomain("192.168.4.2")).toEqual(domain1);
+  });
+  test("Invalid IP = abc.def.ghi.jk", () => {
+    const ip = "abc.def.ghi.jk";
+    expect(() => {
+      new InnateDomain(ip);
+    }).toThrow("abc.def.ghi.jk is an invalid IP Address!");
+  });
+  test("Invalid IP = 1923.168.4.2", () => {
+    const ip = "1923.168.4.2";
+    expect(() => {
+      new InnateDomain(ip);
+    }).toThrow("1923.168.4.2 is an invalid IP Address!");
+  });
+  test("Invalid IP = 192.168.4.4", () => {
+    const ip = "192.168.4.4";
+    expect(() => {
+      new InnateDomain(ip);
+    }).toThrow("was not found in InnateDomainIPMap");
+  });
+});
+
+describe("Innate Domain Methods", () => {
+  describe("fetchProperty", () => {
+    test("Get IP of domain1", () => {
+      expect(domain1.fetchProperty("ip")).toBe("192.168.4.2");
+    });
+    test("Get index of domain1", () => {
+      expect(domain1.fetchProperty("index")).toBe(0);
+    });
+    test("Get 'foo' of domain1", () => {
+      expect(domain1.fetchProperty("foo")).toThrow(
+        "does not exist in an InnateDomain"
+      );
+    });
   });
 });
