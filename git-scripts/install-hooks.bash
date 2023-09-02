7d0a249ae24955d2ede27d4304b99c8989113645
Jonathan <jmyeh51@gmail.com>
2023-09-01 23:37:06 -0700

    Testing script

--- git-scripts/install-hooks.bash
+++ git-scripts/install-hooks.bash
@@ -2,7 +2,7 @@
 
 GIT_DIR=$(git rev-parse --git-dir)
 
-echo "Installing hooks.."
+echo "Installing hooks..."
 # this command creates symlink to our pre-commit script
 ln -s ../../scripts/pre-commit.bash $GIT_DIR/hooks/pre-commit
 echo "Done"!
