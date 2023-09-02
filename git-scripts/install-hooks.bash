2c453f37b0fb7bf2d60de1a73ccce53eec367699
Jonathan <jmyeh51@gmail.com>
2023-09-01 23:49:57 -0700

    Script to run tests before commit

--- git-scripts/install-hooks.bash
+++ git-scripts/install-hooks.bash
@@ -2,7 +2,7 @@
 
 GIT_DIR=$(git rev-parse --git-dir)
 
-echo "Installing hooks..."
+echo "Installing hooks.."
 # this command creates symlink to our pre-commit script
 ln -s ../../git-scripts/pre-commit.bash $GIT_DIR/hooks/pre-commit
 chmod ug+x ../.git/hooks/*
