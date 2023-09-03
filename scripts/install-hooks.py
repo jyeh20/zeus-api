07704ed31d2db14c1f45b04c854c70a5115b1828
Jonathan <jmyeh51@gmail.com>
2023-09-03 12:27:48 -0700

    Adds a pre-push hook to remove merged local branches.

--- scripts/install-hooks.py
+++ scripts/install-hooks.py
@@ -1,12 +1,20 @@
 from os import system
 from os.path import realpath, dirname
 
+GIT_HOOKS = "../.git/hooks"
 this_file = realpath(dirname(__file__))
 
-with open(f"{this_file}/../.git/hooks/pre-commit", "w") as pre_commit:
+with open(f"{this_file}/{GIT_HOOKS}/pre-commit", "w") as pre_commit:
   pre_commit.writelines(
     ["echo Running pre-commit hook\n",
      "npm run test\n"]
   )
+
+with open(f"{this_file}/{GIT_HOOKS}/pre-push", "w") as pre_push:
+  pre_push.writelines([
+    "echo Running pre-push hook\n",
+    "git gc --aggressive\n",
+    "git remote prune origin\n"
+  ])
 
 system(f"chmod ug+x {this_file}//../.git/hooks/*")
