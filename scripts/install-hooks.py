1fad7c32dca4772c015de2e8569b5f80c3308839
Jonathan <jmyeh51@gmail.com>
2023-09-03 12:32:42 -0700

    Adds command line echo's to clarify what hooks are being called.

--- scripts/install-hooks.py
+++ scripts/install-hooks.py
@@ -7,13 +7,16 @@ this_file = realpath(dirname(__file__))
 with open(f"{this_file}/{GIT_HOOKS}/pre-commit", "w") as pre_commit:
   pre_commit.writelines(
     ["echo Running pre-commit hook\n",
+     "echo \nRunning npm run test\n\n",
      "npm run test\n"]
   )
 
 with open(f"{this_file}/{GIT_HOOKS}/pre-push", "w") as pre_push:
   pre_push.writelines([
     "echo Running pre-push hook\n",
+    "echo '\\nRunning git gc --aggressive\\n'\n"
     "git gc --aggressive\n",
+    "echo '\\nRunning git remote prune origin\\n'\n"
     "git remote prune origin\n"
   ])
 
