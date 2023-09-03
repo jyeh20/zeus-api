8a5b2f48eadbab802a63b0311afb78df22103c76
Jonathan <jmyeh51@gmail.com>
2023-09-02 17:01:57 -0700

    Moved consts to individual project folders

--- consts/consts.ts
+++ consts/consts.ts
@@ -1,27 +1,5 @@
-type IP = {
-  [key: string]: number;
-};
-
 type Const = {
   [key: string]: number | string;
-};
-
-type DomainMap = {
-  [key: string]: IP;
-};
-
-const InnateDomainIPMap: IP = process.env.test
-  ? {
-      "192.168.4.1": 0,
-    }
-  : {
-      "192.168.4.2": 0,
-      "192.168.4.3": 1,
-      "192.168.6.2": 2,
-    };
-
-export const DomainIPMap: DomainMap = {
-  InnateDomain: InnateDomainIPMap,
 };
 
 export const consts: Const = {
