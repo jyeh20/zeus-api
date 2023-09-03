d3509265db6e3b3062eae131151732568a6c5368
Jonathan Yeh <49797676+jyeh20@users.noreply.github.com>
2023-09-03 12:35:41 -0700

    Adds a pre-push hook to delete old local branches. (#7)
    * Adds a pre-push hook to remove merged local branches.
    
    * Adds command line echo's to clarify what hooks are being called.

--- scripts/install-hooks.py
+++ scripts/install-hooks.py
@@ -1,12 +1,23 @@
 from os import system
 from os.path import realpath, dirname
 
+GIT_HOOKS = "../.git/hooks"
 this_file = realpath(dirname(__file__))
 
-with open(f"{this_file}/../.git/hooks/pre-commit", "w") as pre_commit:
+with open(f"{this_file}/{GIT_HOOKS}/pre-commit", "w") as pre_commit:
   pre_commit.writelines(
     ["echo Running pre-commit hook\n",
+     "echo \nRunning npm run test\n\n",
      "npm run test\n"]
   )
+
+with open(f"{this_file}/{GIT_HOOKS}/pre-push", "w") as pre_push:
+  pre_push.writelines([
+    "echo Running pre-push hook\n",
+    "echo '\\nRunning git gc --aggressive\\n'\n"
+    "git gc --aggressive\n",
+    "echo '\\nRunning git remote prune origin\\n'\n"
+    "git remote prune origin\n"
+  ])
 
 system(f"chmod ug+x {this_file}//../.git/hooks/*")
